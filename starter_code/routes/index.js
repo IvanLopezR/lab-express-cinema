aa085525f0786b10a0efccaddab12a8aead07ab2
Iván López Ruiz <ivan@MacBook-Air-de-Ivan.local>
2019-07-01 09:19:19 +0200

    done

--- starter_code/routes/index.js
+++ starter_code/routes/index.js
@@ -7,13 +7,23 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get("/movies",(req,res,next)=>{
-  res.render("movies");
-});
-
 router.get("/movies", (req, res, next) => {
   Movie.find().then(movies => {
     res.render('movies', {movies})
+  }).catch((err) => {
+    console.log(err)
+  })
+});
+
+router.get("/movie/:ID", (req, res) => {
+  Movie.findById(req.params.ID, (err, movie) => {
+    res.render('movie', movie)
+  });
+})
+
+router.post("/movie/:id", (req, res, next) => {
+  Movie.find(req.params.id).then(movie => {
+    res.render('/movie', {movie})
   }).catch((err) => {
     console.log(err)
   })
